bd760b28696f96134bc74f9d81a24a635a4b1d7f
VitorJustiniano <vitor.mjustiniano@hotmail.com>
2025-04-03 22:03:11 -0300

    exercicio 14

--- exercicios.py
+++ exercicios.py
@@ -20,9 +20,9 @@ import math
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
-raio_circulo = float(input("Digite o raio: "))
-area_circulo = math.pi * raio_circulo ** 2
-print(f"{area_circulo:.2f}")
+#raio_circulo = float(input("Digite o raio: "))
+#area_circulo = math.pi * raio_circulo ** 2
+#print(f"{area_circulo:.2f}")
 
 # #### Strings (`str`)
 
@@ -30,6 +30,12 @@ print(f"{area_circulo:.2f}")
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+#data_usuario = input("insira uma data no formato dd/mm/aaaa: ")
+#lista_dia_mes_ano = data_usuario.split("/")
+#print(f"o elemento 1 é : {lista_dia_mes_ano[0]}")
+#print(f"o elemento 2 é : {lista_dia_mes_ano[1]}")
+#print(f"o elemento 3 é : {lista_dia_mes_ano[2]}")
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
